5f6dcded86aa114ac981aa079bd1ba248cb37a84
Alex <a.behlen@gmail.com>
2013-11-29 16:31:52 -0700

    Added android build

---
+++ CleanTucson/build/CleanTucson/android/bin/AddressDMess-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/home/saasbook/Desktop/adm/CleanTucsonMobile/CleanTucson/build/CleanTucson/android/bin/AddressDMess-debug-unaligned.apk : \
+/home/saasbook/Desktop/adm/CleanTucsonMobile/CleanTucson/build/CleanTucson/android/bin/AddressDMess.ap_ \
+/home/saasbook/Desktop/adm/CleanTucsonMobile/CleanTucson/build/CleanTucson/android/bin/classes.dex \
+/home/saasbook/Desktop/adm/CleanTucsonMobile/CleanTucson/build/CleanTucson/android/libs/cordova-3.1.0.jar \
